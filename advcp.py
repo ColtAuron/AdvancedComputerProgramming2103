997b88e50c102743b650ffdca3b2114b1a7ac022
ColtAuron <coltaurontan@gmail.com>
2023-09-22 11:18:57 +0800

    Act 4 Exercise
    new act exercise

---
+++ advcp.py
@@ -0,0 +1,21 @@
+print('Hello Peeps!')
+print('This is from a line of code')
+a, b, c = 5, 5.5, 'Hello, World!'
+print('\n', a, '\n', b ,'\n', c, '\n')
+print('1. ', (70+9))
+a = 7
+product = 5*a
+print('2. ', product)
+a, b, c = 2, 11, 0
+a = a*2
+b += (c*3)
+c = a * b
+print('3. ', c)
+input1 = int(input('Input first number: '))
+input2 = int(input('Input second number: '))
+print('4. ', (input1 + input2))
+print('5. Squared = ', (3**2), ', cubed = ', (3**3))
+Name = 'Gly'
+print('6. ', Name)
+Apple_price, Orange_price = 20, 20
+print('7. ', (Apple_price == Orange_price))
